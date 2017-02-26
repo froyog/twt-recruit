1c34801e3bf22609a363e7651028d2930250f307
Tom <scarlet0345@gmail.com>
2017-02-26 11:47:26 +0800

    finish all pages, page5 still under testing

--- static/js/2017spring.js
+++ static/js/2017spring.js
@@ -7,21 +7,17 @@ $(function() {
     });
   }
 
-  /*function timeline (listItem) {
-    listItem.eq(0).fadeIn(100);
-    listItem.eq(1).animate({
-      width: "100%"
-    }, 500, function() {
-      if (listItem.next().length) {
-        timeline(listItem.next());
-      }
-    });
-  }*/
-
   $("#warpper").fullpage(
     {
       navigation: true,
       afterLoad: function (anchorLink, index) {
+        if (index === 2) {
+          var dots = $(this).find("img").slice(0, 7).parent(),
+              tl_items = $(this).find("img").slice(7, 14).parent();
+          orderedAnimation(dots, {opacity: 1}, 500);
+          orderedAnimation(tl_items, {opacity: 1}, 500);
+        }
+
         if (index === 3) {
           var msgs = $(this).find("img").slice(1, 5).parent();
           orderedAnimation(msgs, { opacity: 1 }, 550);
@@ -30,10 +26,37 @@ $(function() {
         if (index === 4) {
           var msgs_4 = $(this).find("img").slice(2, 5).parent(),
               msg_details_4 = $(this).find("img").slice(5, 11).parent();
-          orderedAnimation(msgs_4, { opacity: 1 }, 600);
+          orderedAnimation(msgs_4, {opacity: 1}, 600);
           setTimeout(function() {
             orderedAnimation(msg_details_4, {opacity: 1}, 300);
-          }, 1650);
+          }, 1800);
+        }
+
+        if (index === 5) {
+          var msgs_5 = $(this).find("img").slice(0, 2).parent(),
+              msg_details_5 = $(this).find("img").slice(2, 8).parent();
+          orderedAnimation(msgs_5, {opacity: 1}, 600);
+          setTimeout(function() {
+            orderedAnimation(msg_details_5, {opacity: 1}, 400);
+          }, 1200);
+          $(".coder-desc").css({
+            "animation-name": "typing",
+            "animation-duration": "4s",
+            "animation-timing-function": "steps(11, end)"
+          });
+          $(".coder-desc-2").css({
+            "animation-name": "typing",
+            "animation-duration": "4s",
+            "animation-timing-function": "steps(16, end)"
+          });
+          /*$(".coder-desc, .coder-desc-2").css({
+            "animation-name": "blink-caret",
+            "animation-duration": ".5s",
+            "animation-timing-function": "step-end",
+            "animation-itertaion-count": "infinite",
+            "animation-direction": "alternate"
+          });*/
+
         }
       }
     });
